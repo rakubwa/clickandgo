b562cc523d926e357d03af1a058fdfa08bef85c6
rakujp <kwdowik@yahoo.com>
2022-08-31 18:59:03 +0200

    Wrzucam projekt

---
+++ src/main/java/pl/clickandgo/clickandgo/ClickandgoApplication.java
@@ -0,0 +1,13 @@
+package pl.clickandgo.clickandgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickandgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickandgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/clickandgo/clickandgo/ClickandgoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.clickandgo.clickandgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickandgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
